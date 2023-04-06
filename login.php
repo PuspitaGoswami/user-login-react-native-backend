5f0a840e49b64e3af5b8fd7a7a04f0acdeef03d7
Your Name <you@example.com>
2023-04-06 16:40:17 +0100

    Login successful

--- login.php
+++ login.php
@@ -1,23 +1,38 @@
 <?php 
 $CN = mysqli_connect("localhost", "root", "", "user-login");
 
-$user_name = $_POST['user_name'];
-$user_pass = $_POST['user_pass'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $post_data = json_decode(file_get_contents('php://input'), true);
 
-$IQ = "SELECT user_name FROM user_list WHERE user_name = '$user_name' AND user_pass = '$user_pass'";
+    if (isset($post_data['user_name']) && isset($post_data['user_pass'])) {
+        $user_name = $post_data['user_name'];
+        $user_pass = $post_data['user_pass'];
 
-$R = mysqli_query($CN, $IQ);
+        $IQ = "SELECT user_name FROM user_list WHERE user_name = '$user_name' AND user_pass = '$user_pass'";
 
-if (mysqli_num_rows($R) > 0) {
-    $token = uniqid();
+        $R = mysqli_query($CN, $IQ);
 
-    $Message = "Login successful";
-    $response[] = array("Message" => $Message, "token" => $token);
+        if (mysqli_num_rows($R) > 0) {
+            $token = uniqid();
+
+            $Message = "Login successful";
+            $response[] = array("Message" => $Message, "token" => $token);
+        } else {
+            $Message = "Invalid credentials";
+            $response[] = array("Message" => $Message);
+        }
+
+        echo json_encode($response);
+    } else {
+        $Message = "Please provide user_name and user_pass values.";
+        $response[] = array("Message" => $Message);
+
+        echo json_encode($response);
+    }
 } else {
-    $Message = "Invalid credentials";
+    $Message = "Invalid request method.";
     $response[] = array("Message" => $Message);
+
+    echo json_encode($response);
 }
-
-echo json_encode($response);
-
 ?>
